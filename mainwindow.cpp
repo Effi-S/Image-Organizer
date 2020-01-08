f3205293fa53f6e5e58e7796ceeb81664bcf3509
Effi-S <effi.szt@gmail.com>
2020-01-08 02:57:20 +0200

    first image to dispay in exact tab

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,14 +1,22 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
-
-
+#include <QLabel>
+#include <QScrollArea>
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
     ui->commandLinkButton->setEnabled(false);
+    ui->toolBar->setVisible(false);
     ui->progressBar->setVisible(false);
+
+    QLabel *imageLabel = new QLabel;
+    QImage image("C:\\Users\\effi\\Desktop\\temp\\Player_01.png");
+    imageLabel->setPixmap(QPixmap::fromImage(image));
+
+    ui->scrollArea_exact_tab->setBackgroundRole(QPalette::Dark);
+    ui->scrollArea_exact_tab->setWidget(imageLabel);
 
 
 }
@@ -70,14 +78,16 @@ void MainWindow::on_actionUndo_triggered()
 
 void MainWindow::on_actionToggle_Toolbar_triggered()
 {
-    ui->statusBar->showMessage("Toggle Toolbar");
+    static bool state = false;
+    state = !state;
+    ui->statusBar->showMessage("toggled toolbar");
+    ui->toolBar->setVisible(state);
 }
 
 void MainWindow::on_actionsave_changes_triggered()
 {
     ui->statusBar->showMessage("Save Changes");
 }
-
 
 void MainWindow::on_actionScan_triggered()
 {
@@ -93,10 +103,11 @@ void MainWindow::on_actionScan_triggered()
     connect(m_scanThread.get(), &ScanThread::resultReady, this, &MainWindow::windowHandle);
     //connect(m_scanThread.get(), &ScanThread::finished, m_scanThread, &QObject::deleteLater);
     connect(m_scanThread.get() , &ScanThread::scanDone , ui->commandLinkButton, &QCommandLinkButton::setEnabled);
+    //connect(m_scanThread.get(), &ScanThread::scanDone, ui->statusBar, &QStatusBar::showMessage );
     m_scanThread->start();
-
-
     ui->statusBar->showMessage( "Done Scanning:" + m_currDir);
+
+    //
     ui->progressBar->setValue(100);
     ui->progressBar->reset();
     ui->progressBar->setVisible(false);
@@ -115,3 +126,4 @@ void MainWindow::on_FolderButton_clicked(){
     on_actionchange_file_triggered();
 
 }
+
